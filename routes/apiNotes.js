faff2f3d542365ee42d3a21de3a988f50fd1c9d2
Rogerson Jean-Charles <Rogerson253@Gmail.com>
2020-04-19 20:16:53 -0500

    created basic layout for reading db json

---
+++ routes/apiNotes.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+
+module.exports = (app, fs) => {
+    app.get("/api/notes", (req, res) => {
+
+    });
+};
--- server.js
+++ server.js
@@ -3,9 +3,7 @@
 // Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
 
-var express = require("express");
-
-
+const express = require("express");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -13,10 +11,10 @@ var express = require("express");
 // ==============================================================================
 
 // Tells node that we are creating an "express" server
-var app = express();
+const app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -28,6 +26,7 @@ app.use(express.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 require("./routes/htmlRoutes")(app);
+require("./routes/apiNotes")(app, fs)
 
 // =============================================================================
 // LISTENER
