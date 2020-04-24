83b412f9192fe39de9e901b3f932f5d1df682a01
Rogerson Jean-Charles <Rogerson253@Gmail.com>
2020-04-23 23:02:04 -0500

    created method that deletes notes

--- routes/apiNotes.js
+++ routes/apiNotes.js
@@ -1,31 +1,47 @@
-let fs = require("fs");
-let router = require("express").Router()
-let util = require('util');
+// Dependencies
+const router = require("express").Router()
+const fs = require("fs");
+const util = require('util');
 const uuid1 = require("uuid/v1");
-let readFileAsync = util.promisify(fs.readFile);
-let writeFileAsync = util.promisify(fs.writeFile);  
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
-router.get("/api/notes", async function(req, res) {
+// This method retrieves data from db.json
+router.get("/api/notes", async function (req, res) {
     const readNotes = await readFileAsync("./db/db.json", "utf8");
     res.send(JSON.parse(readNotes))
 });
 
-router.post("/api/notes", async function(req, res) {
-
+// This method takes in user input and stores it in db.json 
+router.post("/api/notes", async function (req, res) {
     const readNotes = await readFileAsync("./db/db.json", "utf8");
+    
     let posting = JSON.parse(readNotes);
     let newNote = {
-    id: uuid1(),
-    title: req.body.title,
-    text: req.body.text
+        id: uuid1(),
+        title: req.body.title,
+        text: req.body.text
     };
 
     posting.push(newNote);
-    console.log(posting);
-    console.log("===============")
-    let done = await writeFileAsync("./db/db.json", JSON.stringify(posting));
+    const done = await writeFileAsync("./db/db.json", JSON.stringify(posting));
 
-    res.send("Note Sent!");
+    res.send(done);
+});
+
+// This method deletes data out of db json by targeting its id
+router.delete("/api/notes/:id", async function (req, res) {
+    const readNotes = JSON.parse(await readFileAsync("./db/db.json", "utf8"));
+   
+    let noteArr = [];
+    for (let i = 0; i < readNotes.length; i++) {
+        if (readNotes[i].id !== req.params.id) {
+            noteArr.push(readNotes[i])
+        };
+    };
+
+    const done = await writeFileAsync("./db/db.json", JSON.stringify(noteArr));
+    res.send(done);
 });
 
 module.exports = router;
